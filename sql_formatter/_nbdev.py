ee6017d006c2f687691488a25b19d0cf88113b64
Pablo Rodriguez Mira <36644554+PabloRMira@users.noreply.github.com>
2020-12-16 23:03:45 +0100

    [FIX] SELECT adds newline for function arguments and outsource auxiliary functions to utils (#35)
    This closes #28

--- sql_formatter/_nbdev.py
+++ sql_formatter/_nbdev.py
@@ -2,15 +2,11 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"assert_and_print": "00_core.ipynb",
-         "MAIN_STATEMENTS": "00_core.ipynb",
-         "remove_newlines_mspaces": "00_core.ipynb",
-         "preformat_statement": "00_core.ipynb",
+index = {"MAIN_STATEMENTS": "00_core.ipynb",
+         "clean_query": "00_core.ipynb",
+         "preformat_statements": "00_core.ipynb",
          "lowercase_query": "00_core.ipynb",
-         "add_whitespaces_between_symbols": "00_core.ipynb",
-         "format_select_partition_by": "00_core.ipynb",
          "format_select": "00_core.ipynb",
-         "format_partition_by": "00_core.ipynb",
          "format_from": "00_core.ipynb",
          "format_join": "00_core.ipynb",
          "format_on": "00_core.ipynb",
@@ -19,17 +15,32 @@ index = {"assert_and_print": "00_core.ipynb",
          "format_statements": "00_core.ipynb",
          "add_ending_semicolon": "00_core.ipynb",
          "format_simple_sql": "00_core.ipynb",
-         "extract_outer_subquery": "00_core.ipynb",
-         "format_subquery": "00_core.ipynb",
          "format_sql": "00_core.ipynb",
-         "check_sql_query": "01_format_file.ipynb",
-         "check_skip_marker": "01_format_file.ipynb",
          "format_sql_commands": "01_format_file.ipynb",
          "format_sql_file": "01_format_file.ipynb",
-         "format_sql_files": "01_format_file.ipynb"}
+         "format_sql_files": "01_format_file.ipynb",
+         "assert_and_print": "02_utils.ipynb",
+         "remove_whitespaces_newline": "02_utils.ipynb",
+         "remove_whitespaces_comments": "02_utils.ipynb",
+         "remove_redundant_whitespaces": "02_utils.ipynb",
+         "mark_comments": "02_utils.ipynb",
+         "identify_queries": "02_utils.ipynb",
+         "identify_newline_chars": "02_utils.ipynb",
+         "replace_newline_chars": "02_utils.ipynb",
+         "identify_select_from": "02_utils.ipynb",
+         "split_by_select_from": "02_utils.ipynb",
+         "add_whitespaces_between_symbols": "02_utils.ipynb",
+         "identify_end_of_fields": "02_utils.ipynb",
+         "add_newline_indentation": "02_utils.ipynb",
+         "extract_outer_subquery": "02_utils.ipynb",
+         "format_subquery": "02_utils.ipynb",
+         "check_sql_query": "02_utils.ipynb",
+         "check_skip_marker": "02_utils.ipynb",
+         "split_by_query": "02_utils.ipynb"}
 
 modules = ["core.py",
-           "format_file.py"]
+           "format_file.py",
+           "utils.py"]
 
 doc_url = "https://PabloRMira.github.io/sql_formatter/"
 
